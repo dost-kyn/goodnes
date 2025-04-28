c10f8a360e0177311b13d94a36f4ec185eb7fdce
Ваше имя <dostoevskijf700@gmail.com>
2025-04-29 01:36:09 +0400

    19/04

---
+++ js/home.js
@@ -0,0 +1,45 @@
+const toggleTheme = document.querySelector(".header_nav_tema"); //берем кнопку по смене темы
+let currentTheme = localStorage.getItem('theme') || 'light'; //Если значение 'theme' - ложно, вернет 'light'
+
+function updateThemeImages(theme) {
+  const themeImages = document.querySelectorAll('[data-theme-image]'); // все объекты с параметром data-theme-image
+  
+  themeImages.forEach(img => {
+    if (!img.dataset.light || !img.dataset.dark) {  //Обращение к data-атрибутам элемента <img>: data-light="/image/cake_icon.svg"  data-dark="/image/cake_icon-dark.svg"
+      console.warn('Отсутствуют data-атрибуты у:', img);
+      return;
+    }
+
+    
+    let newSrc;
+    if (theme === 'dark') {
+      newSrc = img.dataset.dark;
+    } else {
+      newSrc = img.dataset.light;
+    }
+    
+    // Меняем src если нужно
+    if (img.src !== newSrc) {
+        img.src = newSrc;
+    }
+  });
+}
+
+// Инициализация темы
+function initTheme() {
+  if (currentTheme === 'dark') {
+    document.documentElement.setAttribute('data-theme', 'dark');
+  }
+  updateThemeImages(currentTheme);
+}
+
+// Клик по переключателю
+toggleTheme.addEventListener('click', () => {
+  currentTheme = currentTheme === 'dark' ? 'light' : 'dark';
+  localStorage.setItem('theme', currentTheme);
+  document.documentElement.setAttribute('data-theme', currentTheme);
+  updateThemeImages(currentTheme);
+});
+
+// Запуск при загрузке
+document.addEventListener('DOMContentLoaded', initTheme);
